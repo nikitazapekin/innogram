434780375eedb9337fc46ceb0b0925d1132561ff
Nikita <nikitazapekin>
2026-04-07 13:52:55 +0300

    chore(gitignore): Update gitignore

--- apps/core_microservice/src/app.module.ts
+++ apps/core_microservice/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-
-import { AuthModule } from './auth/auth.module';
+ 
 import { HealthController } from './health.controller';
-import { PostsModule } from './posts/posts.module';
+ 
 
 @Module({
-  imports: [AuthModule, PostsModule],
+  
   controllers: [HealthController],
 })
 export class AppModule {}
