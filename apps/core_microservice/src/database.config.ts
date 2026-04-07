337d35bc37be3174d15771d452d8367a7df188c6
Nikita <nikitazapekin>
2026-04-07 15:16:02 +0300

    feat(migrations): Add schema creature and scripts for creature

--- apps/core_microservice/src/database.config.ts
+++ apps/core_microservice/src/database.config.ts
@@ -1,14 +1,24 @@
+import 'reflect-metadata';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
+const databaseConfig: PostgresConnectionOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST ?? 'localhost',
- port: parseInt(process.env.POSTGRES_PORT ?? "5432"),
+  port: parseInt(process.env.POSTGRES_PORT ?? '5432', 10),
   username: process.env.POSTGRES_USER ?? 'postgres',
   password: process.env.POSTGRES_PASSWORD ?? 'postgres',
   database: process.env.POSTGRES_DATABASE ?? 'innogram',
+};
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  ...databaseConfig,
   autoLoadEntities: true,
   synchronize: false,
 };
 
- 
+export default new DataSource({
+  ...databaseConfig,
+  migrations: ['src/database/migrations/*.ts'],
+});
