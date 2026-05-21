e375b1cf401180bf11d76f9848e71b7f8e922e03
Nikita <nikitazapekin>
2026-05-21 18:01:01 +0300

    fix(creature): Fix user creature

--- apps/core/src/users/users.controller.ts
+++ apps/core/src/users/users.controller.ts
@@ -19,10 +19,12 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 
+import { Public } from '@innogram/shared';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserDto } from './dto/user.dto';
 import { UsersService } from './users.service';
 
+@Public()
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
