782ba5278de7486ad5b70a0c3aad8a3c5495a03c
Nikita <nikitazapekin>
2026-05-22 03:04:32 +0300

    feat(validations): Add validations

--- apps/core/src/posts/posts.service.ts
+++ apps/core/src/posts/posts.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -38,6 +43,14 @@ export class PostsService {
       throw new UnauthorizedException('Authenticated user was not found.');
     }
 
+    if (!createPostDto.title) {
+      throw new BadRequestException('Title must not be empty or contain only whitespace.');
+    }
+
+    if (!createPostDto.content) {
+      throw new BadRequestException('Content must not be empty or contain only whitespace.');
+    }
+
     const post = this.postsRepository.create({
       authorProfileId: user.id,
       title: createPostDto.title,
@@ -52,11 +65,25 @@ export class PostsService {
   async updatePost(id: number, updatePostDto: UpdatePostDto): Promise<PostDto> {
     const post = await this.findPostById(id);
 
+    if (updatePostDto.title === undefined && updatePostDto.content === undefined) {
+      throw new BadRequestException(
+        'At least one field (title or content) must be provided for update.',
+      );
+    }
+
     if (updatePostDto.title !== undefined) {
+      if (!updatePostDto.title) {
+        throw new BadRequestException('Title must not be empty or contain only whitespace.');
+      }
+
       post.title = updatePostDto.title;
     }
 
     if (updatePostDto.content !== undefined) {
+      if (!updatePostDto.content) {
+        throw new BadRequestException('Content must not be empty or contain only whitespace.');
+      }
+
       post.content = updatePostDto.content;
     }
 
