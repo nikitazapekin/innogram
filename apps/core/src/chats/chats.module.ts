eca1690547110cb7dfb88d87720b375f180bff32
Nikita <nikitazapekin>
2026-05-28 13:33:29 +0300

    feat(file): Add file attachment to message

--- apps/core/src/chats/chats.module.ts
+++ apps/core/src/chats/chats.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Asset } from '../entities/asset.entity';
 import { Chat } from '../entities/chat.entity';
 import { Message } from '../entities/message.entity';
+import { ChatsController } from './chats.controller';
 import { ChatsGateway } from './chats.gateway';
 import { ChatsService } from './chats.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Chat, Message])],
+  imports: [TypeOrmModule.forFeature([Asset, Chat, Message])],
+  controllers: [ChatsController],
   providers: [ChatsGateway, ChatsService],
 })
 export class ChatsModule {}
