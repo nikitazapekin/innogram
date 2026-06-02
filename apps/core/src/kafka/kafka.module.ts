5ef1ba1aa48433c736b64cdaf107aec829dd9197
Nikita <nikitazapekin>
2026-06-02 17:55:53 +0300

    feat(notifications): Add notifications

--- apps/core/src/kafka/kafka.module.ts
+++ apps/core/src/kafka/kafka.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 
 import { KafkaService } from './kafka.service';
+import { NotificationEventsProducer } from './notification-events.producer';
 
 @Global()
 @Module({
-  providers: [KafkaService],
-  exports: [KafkaService],
+  providers: [KafkaService, NotificationEventsProducer],
+  exports: [KafkaService, NotificationEventsProducer],
 })
 export class KafkaModule {}
--- apps/notifications/src/app.module.ts
+++ apps/notifications/src/app.module.ts
@@ -2,26 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { Notification } from './entities/notification.entity';
-import { MentionConsumer } from './kafka/mention.consumer';
-import { NotificationsController } from './notifications.controller';
+import { NotificationsDatabaseConfigService } from './database.config';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      entities: [Notification],
-      synchronize: false,
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      ignoreEnvFile: true,
+      isGlobal: true,
     }),
-    TypeOrmModule.forFeature([Notification]),
+    TypeOrmModule.forRootAsync({
+      useClass: NotificationsDatabaseConfigService,
+    }),
+    NotificationsModule,
   ],
-  controllers: [NotificationsController],
-  providers: [MentionConsumer],
 })
 export class AppModule {}
