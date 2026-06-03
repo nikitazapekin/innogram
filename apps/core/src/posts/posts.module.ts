2e718027865839846765dea9bd6392678f701710
Nikita <nikitazapekin>
2026-06-03 20:13:04 +0300

    feat(nention): Add mention

--- apps/core/src/posts/posts.module.ts
+++ apps/core/src/posts/posts.module.ts
@@ -4,11 +4,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from '../entities/post.entity';
 import { UserEntity } from '../entities/user.entity';
 import { ArchivedPost } from '../entities/archived-post.entity';
+import { Notification } from '../entities/notification.entity';
+import { KafkaModule } from '../kafka/kafka.module';
+import { MentionsModule } from '../mentions/mentions.module';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, UserEntity, ArchivedPost])],
+  imports: [
+    TypeOrmModule.forFeature([Post, UserEntity, ArchivedPost, Notification]),
+    MentionsModule,
+    KafkaModule,
+  ],
   controllers: [PostsController],
   providers: [PostsService],
 })
