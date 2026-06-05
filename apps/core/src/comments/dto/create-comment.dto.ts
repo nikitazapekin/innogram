290b15c323bce15bc168937c871c1157768f7eb8
Nikita <nikitazapekin>
2026-06-05 17:24:54 +0300

    feat(comments): make parentId optional for top-level comments

--- apps/core/src/comments/dto/create-comment.dto.ts
+++ apps/core/src/comments/dto/create-comment.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class CreateCommentDto {
@@ -9,7 +9,8 @@ export class CreateCommentDto {
   @IsString()
   content: string;
 
+  @IsOptional()
   @IsNumber()
   @Type(() => Number)
-  parentId: number;
+  parentId?: number;
 }
