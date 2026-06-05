a9fe9f83b6d5f58eb8382fecaac5a7e722ce3f29
Nikita <nikitazapekin>
2026-06-05 15:21:17 +0300

    chore(comment): Remove comment

--- apps/client_app/app/widgets/feed/Feed.tsx
+++ apps/client_app/app/widgets/feed/Feed.tsx
@@ -29,12 +29,10 @@ export function Feed() {
   const [profileId, setProfileId] = useState<number | null>(null);
 
   useEffect(() => {
-    getProfile()
-      .then((profile) => {
-        const pid = profile?.id;
-        if (pid) setProfileId(pid);
-      })
-      .catch(() => {});
+    getProfile().then((profile) => {
+      const pid = profile?.id;
+      if (pid) setProfileId(pid);
+    });
   }, []);
 
   const fetchPosts = async () => {
